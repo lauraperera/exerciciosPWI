fbee089ce23ecb18f567d4ed9020ecad84a82ecc
Laura Perera <lauraperera.pss@gmail.com>
2021-10-14 17:01:28 -0300

    Chamando uma função que faz soma em JS

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 //console.log("Hello World!")
 
-var nome = 'Laura'
-var sobrenome = 'Perera'
+/*var nome = 'Laura'
+var sobrenome = Perera'
 console.log('Meu nome completo é: '+nome+' '+sobrenome)
-console.log(`Meu nome completo é: ${nome} ${sobrenome}`)
+console.log(`Meu nome completo é: ${nome} ${sobrenome}`)*/
+
+const SomaFunc = require('./funcaoSomar') //./funcaoSomar no mesmo diretorio
+
+var numero1 = 10
+var numero2 = 20
+
+console.log('O valor da soma é '+(SomaFunc(numero1, numero2)))
